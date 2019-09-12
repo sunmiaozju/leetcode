7f30705ce4fd6202a1e8444b774e830b3487f93d
sunmiaozju <sun_miao@zju.edu.cn>
2019-09-12 14:10:14 +0800

    update

--- 107.binary-tree-level-order-traversal-ii.cpp
+++ 107.binary-tree-level-order-traversal-ii.cpp
@@ -38,18 +38,76 @@
  * 
  * 
  */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
+//   Definition for a binary tree node.
+// struct TreeNode {
+//     int val;
+//     TreeNode* left;
+//     TreeNode* right;
+//     TreeNode(int x)
+//         : val(x)
+//         , left(NULL)
+//         , right(NULL)
+//     {
+//     }
+// };
+
+#include <queue>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root)
     {
+        // 广度优先遍历
+        // if (root == NULL) {
+        //     return vector<vector<int>>();
+        // }
+        // TreeNode* node;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // vector<vector<int>> ans;
+        // while (!q.empty()) {
+        //     int num = q.size();
+        //     vector<int> tmp;
+        //     for (size_t i = 0; i < num; i++) {
+        //         node = q.front();
+        //         q.pop();
+        //         tmp.push_back(node->val);
+        //         if (node->left) {
+        //             q.push(node->left);
+        //         }
+        //         if (node->right) {
+        //             q.push(node->right);
+        //         }
+        //     }
+        //     ans.insert(ans.begin(), tmp);
+        // }
+        // return ans;
+
+        // 递归解法
+        int level = maxLength(root);
+        vector<vector<int>> ans(level, vector<int>());
+        levelOrder(ans, root, level - 1);
+        return ans;
+    }
+    void levelOrder(vector<vector<int>>& ans, TreeNode* root, int level)
+    {
+        if (!root) {
+            return;
+        }
+        ans[level].push_back(root->val);
+        levelOrder(ans, root->left, level - 1);
+        levelOrder(ans, root->right, level - 1);
+    }
+
+    int maxLength(TreeNode* root)
+    {
+        if (!root) {
+            return 0;
+        }
+        return max(maxLength(root->left), maxLength(root->right)) + 1;
     }
 };
